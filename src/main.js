ff47d31d8180333425a3e7132d7f49eef5bff58a
Patrick Scaramal <p.scaramal@reply.com>
2023-05-12 17:46:43 -0300

    inclusao de novo cenario

--- src/main.js
+++ src/main.js
@@ -1,20 +1,32 @@
 import yargs from 'yargs/yargs';
 import insereUnicaNulos from './service/unica_nulos.js';
+import insereUnicaNulosIndex from './service/unica_nulos_index.js';
  
 let argv = yargs(process.argv.slice(2)).argv;
 
 function validaEntrada(quantidade){
     if (isNaN(quantidade)){
-        console.log("Quantidade deve ser um número");
-        return;
+        console.log("Quantidade deve ser um valor numérico");
+        process.exit(1);
     }
 }
 
-async function insereDados(quantidade){
-    insereUnicaNulos(quantidade);
+async function insereDados(quantidade, cenario){
+    switch (cenario) {
+        case 'unica_nulos':
+            await insereUnicaNulos(quantidade);
+            break;
+        case 'unica_nulos_index':
+            await insereUnicaNulosIndex(quantidade);
+            break;
+        default:
+            console.log('cenario inválido: ' + cenario);
+            process.exit(1);
+    }
 }
 
 const qtd = argv.qtd;
+const cen = argv.cenario;
 
 validaEntrada(qtd);
-await insereDados(qtd);
+await insereDados(qtd, cen);
